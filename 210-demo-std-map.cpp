29fcb28ec95a97d98fbdbd793baec1422290e082
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-30 20:56:09 -0700

    Milestone 4

--- 210-demo-std-map.cpp
+++ 210-demo-std-map.cpp
@@ -13,54 +13,11 @@ void MainMenu(map<string, tuple<int,string,string>>& Villager);
 int main() {
     // declarations
     map<string, tuple<int,string,string>> Villager;
-  
-    
-    // insert elements into the map
-    // note how the right-hand side of the assignment are the vector elements
-   
     Villager["Audie"] = {5, "Alligator", "Snap to it!"};
     Villager["Raymond"] = {10, "Wolf", "woof woof"};
     Villager.insert({"Marshal", {8, "Cat", "cyaat"}});
     //Main menu Function Handles all of the operations needed.
     MainMenu(Villager);
-/*
-    // access the map using a range-based for loop
-    cout << "Villagers details:" << endl;
-    for (auto[pair,villagerdeets] : Villager ) {
-        auto[happiness,species,phrase]=villagerdeets;
-        cout << pair<< "["<<happiness<<", "<<species<<", "<<phrase<<"]"<<endl;
-    }
-
-    // access the map using iterators
-    cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, tuple<int,string,string>>::iterator it = Villager.begin(); it != Villager.end(); ++it) {
-        cout << it->first << "[";
-        tuple<int,string,string>villagerdeets=it->second;
-        cout<<get<0>(villagerdeets)<<", "<<get<1>(villagerdeets)<<", "<<get<2>(villagerdeets)<<"]";
-        cout << endl;
-    }
-
-    // delete an element
-    Villager.erase("Raymond");
-
-    // search for an element using .find() to avoid errors
-    string searchKey = "Audie";
-    auto it = Villager.find(searchKey);
-    if (it != Villager.end()) {  // the iterator points to beyond the end of the map
-                                       // if searchKey is not found
-        cout << "\nFound " << searchKey << "'s Villager details! "<<endl;
-        tuple<int,string,string>villagerdeets=it->second;
-        cout<<"["<<get<0>(villagerdeets)<<", "<<get<1>(villagerdeets)<<", "<<get<2>(villagerdeets)<<"]";
-        
-        cout << endl;
-    } else
-        cout << endl << searchKey << " not found." << endl;
-
-    // report size, clear, report size again to confirm map operations
-    cout << "\nSize before clear: " << Villager.size() << endl;
-    Villager.clear();
-    cout << "Size after clear: " << Villager.size() << endl;
-    */
     return 0;
 }
 
@@ -106,10 +63,23 @@ void MainMenu(map<string, tuple<int,string,string>>& Villager){
             string temp4;
             getline(cin,temp4);
             cout<<temp<<" was added!"<<endl;
+            cout<<endl;
             Villager.insert({temp,{temp2,temp3,temp4}});
         }
         if(response==2){
-
+            cout<<endl;
+            cout<<"Which Villager to Delete?"<<endl;
+            for (auto[pair,villagerdeets] : Villager ) {
+                auto[happiness,species,phrase]=villagerdeets;
+                cout << pair<< "["<<happiness<<", "<<species<<", "<<phrase<<"]"<<endl;
+            }
+            cin.ignore();
+            cout<<"Enter Villager Name: ";
+            string temp;
+            getline(cin,temp);
+            Villager.erase(temp);
+            cout<<"Villager deleted"<<endl;
+            cout<<endl;
         }
         if(response==3){
             cout<<endl;
@@ -190,7 +160,7 @@ void MainMenu(map<string, tuple<int,string,string>>& Villager){
         break;
        }
         else{
-            cout<<"Invalid response please try again"<<endl;
+            
         }
     cout << "Villagers details:" << endl;
     for (auto[pair,villagerdeets] : Villager ) {
