46f5b2acd7a89d47abc5dc1e2851eaa9af66369c
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-10-30 19:41:45 -0700

    7:41 PM

--- 210-demo-std-map.cpp
+++ 210-demo-std-map.cpp
@@ -29,12 +29,11 @@ int main() {
 
     // access the map using iterators
     cout << "\nVillagers and their favorite colors (iterators):" << endl;
-    for (map<string, tuple<int,string,string>>::iterator it = Villager.begin(); 
-                                               it != Villager.end(); ++it) {
-        cout << it->first << ": ";
-        for (auto color : it->second) {
-           
-        }
+    for (map<string, tuple<int,string,string>>::iterator it = Villager.begin(); it != Villager.end(); ++it) {
+        cout << it->first << "[";
+        tuple<int,string,string>villagerdeets=it->second;
+        cout<<get<0>(villagerdeets)<<", "<<get<1>(villagerdeets)<<", "<<get<2>(villagerdeets)<<"]";
+
         cout << endl;
     }
 
@@ -43,20 +42,20 @@ int main() {
 
     // search for an element using .find() to avoid errors
     string searchKey = "Audie";
-    auto it = villagerColors.find(searchKey);
-    if (it != villagerColors.end()) {  // the iterator points to beyond the end of the map
+    auto it = Villager.find(searchKey);
+    if (it != Villager.end()) {  // the iterator points to beyond the end of the map
                                        // if searchKey is not found
         cout << "\nFound " << searchKey << "'s favorite colors: ";
-        for (auto color : it->second)  // range loop to traverse the value/vector
+        for (auto tuple<int,string,string>villagerdeets : it->second)  // range loop to traverse the value/vector
             cout << color << " ";
         cout << endl;
     } else
         cout << endl << searchKey << " not found." << endl;
 
     // report size, clear, report size again to confirm map operations
-    cout << "\nSize before clear: " << villagerColors.size() << endl;
-    villagerColors.clear();
-    cout << "Size after clear: " << villagerColors.size() << endl;
+    cout << "\nSize before clear: " << Villager.size() << endl;
+    Villager.clear();
+    cout << "Size after clear: " << Villager.size() << endl;
 
     return 0;
 }
